875398f6a2f31d6ac2e05058aa65f212181462d9
Michael Tobin <mikel.tobin@gmail.com>
2016-11-17 17:57:36 -0700

    Finished leap, exercism #2

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,7 +1,25 @@
-var Leap = function() {};
-
-Leap.prototype.isLeap = function(input) {
-  
+var Leap = function(year) {
+  Leap.prototype.isLeap = function(input) {
+    if(year % 4 === 0){
+      if(year % 100 === 0){
+        if(year % 400 === 0){
+          return true
+        }
+        return false
+      }else{
+        return true
+      }
+    }else{
+      return false
+    }
+    return false
+  }
 };
+// `plain
+// on every year that is evenly divisible by 4
+//   except every year that is evenly divisible by 100
+//     unless the year is also evenly divisible by 400
+// ```
+
 
 module.exports = Leap;
